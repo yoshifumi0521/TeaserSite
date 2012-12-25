e8d3b85ea7019977513ae614b7834538de619aa6
YoshifumiSato <yoshifumi0521@gmail.com>
2012-12-25 15:17:09 +0900

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TeaserSite::Application.config.secret_token = '838c55ac913cf995b3cedaeed50067ea34339df73a020169189ccd0e6938057135c132a7cc667d2756e65469f10d6164edcf303695e93178e7d114d5c11f0e4b'
