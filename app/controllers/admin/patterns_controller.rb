4fc6beb1c0fda9b79c8bf401d0a29674c2fed532
YoshifumiSato <yoshifumi0521@gmail.com>
2012-12-26 22:24:41 +0900

    テーブルを作成

--- app/controllers/admin/patterns_controller.rb
+++ app/controllers/admin/patterns_controller.rb
@@ -1,21 +1,24 @@
 #coding: utf-8
-class PatternsController < Admin::Base
+class Admin::PatternsController < Admin::Base
   
-  
+  before_filter :login_required
 
   def show
   
+    @pattern = Pattern.find_by_id(params[:id])
+    return raise unless @pattern
+
+    @users = @pattern.users
 
 
 
 
 
-
-
-
-
-  
   end
+
+
+
+
 
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 #coding: utf-8
 TeaserSite::Application.routes.draw do
  
-
   resources :users,only: [:create]
 
   #パターンのルーティング
